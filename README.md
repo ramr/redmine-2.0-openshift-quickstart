384e8de83b520c6709c92c74f496b537b04a11b0
Ram Ranganathan <ramr@redhat.com>
2012-07-09 12:00:18 -0700

    Create quickstart for Redmine 2.0.3 version.

---
+++ README.md
@@ -0,0 +1,44 @@
+Redmine on OpenShift
+=========================
+ 
+Redmine is a flexible project management web application. Written using Ruby on Rails framework, it is cross-platform and cross-database.
+
+Redmine is open source and released under the terms of the GNU General Public License v2 (GPL).
+
+More information can be found on the official Redmine set (www.redmine.org)
+Running on OpenShift
+--------------------
+
+Create an account at http://openshift.redhat.com/
+
+Create a ruby application (ruby-1.8 or ruby-1.9)
+
+	rhc app create -a redmine -t ruby-1.9  # or ruby-1.8
+
+Add mysql support to your application
+    
+	rhc app cartridge add -a redmine -c mysql-5.1
+
+Make a note of the username, password, and host name as you will need to use these to login to the mysql database
+
+Add this upstream Redmine quickstart repo
+
+	cd redmine
+	rm -rf *
+	git remote add upstream -m master git://github.com/ramr/redmine-2.0-openshift-quickstart.git
+	git pull -s recursive -X theirs upstream master
+
+Then push the repo upstream
+
+	git push
+
+That's it, you can now checkout your application at:
+
+	http://redmine-$yournamespace.rhcloud.com
+
+
+Use the following to login to your new Redmine application running on OpenShift:
+
+	username: admin
+	password: admin
+
---
+++ app/models/issue_status.rb
@@ -0,0 +1,104 @@
+# Redmine - project management software
+# Copyright (C) 2006-2012  Jean-Philippe Lang
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+class IssueStatus < ActiveRecord::Base
+  before_destroy :check_integrity
+  has_many :workflows, :foreign_key => "old_status_id"
+  acts_as_list
+
+  before_destroy :delete_workflows
+  after_save     :update_default
+
+  validates_presence_of :name
+  validates_uniqueness_of :name
+  validates_length_of :name, :maximum => 30
+  validates_inclusion_of :default_done_ratio, :in => 0..100, :allow_nil => true
+
+  scope :named, lambda {|arg| { :conditions => ["LOWER(#{table_name}.name) = LOWER(?)", arg.to_s.strip]}}
+
+  def update_default
+    IssueStatus.update_all("is_default=#{connection.quoted_false}", ['id <> ?', id]) if self.is_default?
+  end
+
+  # Returns the default status for new issues
+  def self.default
+    find(:first, :conditions =>["is_default=?", true])
+  end
+
+  # Update all the +Issues+ setting their done_ratio to the value of their +IssueStatus+
+  def self.update_issue_done_ratios
+    if Issue.use_status_for_done_ratio?
+      IssueStatus.find(:all, :conditions => ["default_done_ratio >= 0"]).each do |status|
+        Issue.update_all(["done_ratio = ?", status.default_done_ratio],
+                         ["status_id = ?", status.id])
+      end
+    end
+
+    return Issue.use_status_for_done_ratio?
+  end
+
+  # Returns an array of all statuses the given role can switch to
+  # Uses association cache when called more than one time
+  def new_statuses_allowed_to(roles, tracker, author=false, assignee=false)
+    if roles && tracker
+      role_ids = roles.collect(&:id)
+      transitions = workflows.select do |w|
+        role_ids.include?(w.role_id) &&
+        w.tracker_id == tracker.id &&
+        ((!w.author && !w.assignee) || (author && w.author) || (assignee && w.assignee))
+      end
+      transitions.collect{|w| w.new_status}.compact.sort
+    else
+      []
+    end
+  end
+
+  # Same thing as above but uses a database query
+  # More efficient than the previous method if called just once
+  def find_new_statuses_allowed_to(roles, tracker, author=false, assignee=false)
+    if roles.present? && tracker
+      conditions = "(author = :false AND assignee = :false)"
+      conditions << " OR author = :true" if author
+      conditions << " OR assignee = :true" if assignee
+
+      workflows.find(:all,
+        :include => :new_status,
+        :conditions => ["role_id IN (:role_ids) AND tracker_id = :tracker_id AND (#{conditions})",
+          {:role_ids => roles.collect(&:id), :tracker_id => tracker.id, :true => true, :false => false}
+          ]
+        ).collect{|w| w.new_status}.compact.sort
+    else
+      []
+    end
+  end
+
+  def <=>(status)
+    position <=> status.position
+  end
+
+  def to_s; name end
+
+private
+  def check_integrity
+    raise "Can't delete status" if Issue.find(:first, :conditions => ["status_id=?", self.id])
+  end
+
+  # Deletes associated workflows
+  def delete_workflows
+    Workflow.delete_all(["old_status_id = :id OR new_status_id = :id", {:id => id}])
+  end
+end
